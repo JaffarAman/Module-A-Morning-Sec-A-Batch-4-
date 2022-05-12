96aedc125906c60e0d65573da895d9c062b8e5de
umairkhan1997 <amanjaffar50@gmail.com>
2022-05-12 11:30:17 +0500

    javascript function complete

---
+++ class 31/app.js
@@ -0,0 +1,87 @@
+// alert("hello class");
+
+// function sum(parameters) {
+//   console.log(10 + 20);
+// }
+
+// sum(arguments);
+
+// function sum(num1, num2 = 0) {
+//   console.log(num1 + num2);
+//   console.log(num1);
+//   console.log(num2);
+// }
+
+// sum(20);
+
+// function greet(username) {
+//   //   var username = "jaffar aman";
+//   //   console.log(username);
+//   return username;
+// }
+
+// console.log(greet("Jaffar Aman"));
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+//   //   return num1 + num2;
+// }
+
+// var value = sum(20, 50);
+// console.log("value", value);
+
+// function cap(value) {
+//   var capWord = value[0].toUpperCase() + value.slice(1);
+//   return capWord;
+// }
+
+// var value = cap("aman");
+
+// console.log(value);
+
+// function foo() {
+//   console.log("hello");
+//   return;
+//   console.log("world");
+//   return;
+// }
+
+// foo();
+
+// function foo() {
+//   var username = "Jaffar Aman";
+//   console.log(username);
+// }
+// var username = "talha";
+
+// foo();
+// console.log(username);
+
+var username = "jaffar";
+
+/*function foo() {
+    username = "jaffar aman";
+  console.log(username);
+}
+console.log(username);
+
+foo();
+*/
+
+// function greet() {
+//   console.log("hello world");
+//   //   logics
+//   return 10;
+// }
+
+// greet();
+
+// function userName(name) {
+//   alert(name);
+// }
+
+function imgChange() {
+  var carimg = document.getElementById("carimg");
+
+  carimg.src = "./img/car2.jpg";
+}
