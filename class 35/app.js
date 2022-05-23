f21f9c30d929c6d93a80b536a3c9fe371d6e4562
umairkhan1997 <amanjaffar50@gmail.com>
2022-05-23 11:25:32 +0500

    js dom start

---
+++ class 35/app.js
@@ -0,0 +1,22 @@
+// var para = document.getElementById("para");
+// console.log(para);
+
+// var a = document.childNodes[1].childNodes[2].childNodes[3];
+// console.log(a.innerHTML);
+
+// var a = (document.childNodes[1].childNodes[2].childNodes[1].innerHTML =
+//   "HELLO WORLD");
+// console.log(a);
+
+// var a = document.childNodes[1].childNodes[1].childNodes;
+// console.log(a);
+
+// var pappa = document.getElementById("pappa");
+// var firstChild = pappa.firstChild;
+// var lastChild = pappa.lastChild;
+// console.log(lastChild.nextSibling);
+
+// var secondChild = pappa.firstChild.nextSibling.nextSibling.style.color = "red"
+
+var child = document.getElementById("child");
+console.log(child.parentNode);
